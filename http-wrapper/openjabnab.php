0b38222a122dce87321a7118a2d6b602d311047c
jboubechtoula <julien.boubechtoula@amabis.fr>
2015-08-28 17:05:48 +0200

    Ajout 443

--- http-wrapper/openjabnab.php
+++ http-wrapper/openjabnab.php
@@ -1,5 +1,7 @@
 <?php
-	$socket = fsockopen("127.0.0.1", 8080);
+	$socket = fsockopen("127.0.0.1", 433);
+	if(!$socket)
+		$socket = fsockopen("127.0.0.1", 8080);
 	if(!$socket)
 		echo "Problem with OpenJabNab !";
 	else
