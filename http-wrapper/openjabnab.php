440608ea0bfefd74d00d9c2739ed93fd5fee15fe
jboubechtoula <julien.boubechtoula@amabis.fr>
2015-08-28 17:20:17 +0200

    Only HTTPS

--- http-wrapper/openjabnab.php
+++ http-wrapper/openjabnab.php
@@ -1,10 +1,6 @@
 <?php
 	$socket = fsockopen("127.0.0.1", 433);
 	if(!$socket)
-		$socket = fsockopen("127.0.0.1", 80);
-	if(!$socket)
-		$socket = fsockopen("127.0.0.1", 8080);
-	if(!$socket)
 		echo "Problem with OpenJabNab !";
 	else
 	{
